82d546193c3d299dc7883aec39bcba248f4f7232
IvanGavrilov777 <125308112+IvanGavrilov777@users.noreply.github.com>
2023-04-11 09:52:55 +0300

    Update null-provider.tf

--- null-provider.tf
+++ null-provider.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    null = {
-      source = "hashicorp/null"
-      version = "3.2.1"
-    }
-  }
-}
-
 resource "null_resource" "screen_output" {
   provisioner "local-exec" {
     command = <<EOT
