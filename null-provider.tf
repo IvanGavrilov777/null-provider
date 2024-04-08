3f4e88d175495975a3a8dee5654ccdb945502361
IvanGavrilov777 <125308112+IvanGavrilov777@users.noreply.github.com>
2024-04-08 12:38:00 +0300

    Update null-provider.tf

--- null-provider.tf
+++ null-provider.tf
@@ -1,7 +1,10 @@
 resource "null_resource" "screen_output" {
   provisioner "local-exec" {
     command = <<EOT
-    date
+    bucket="ivan-timebucket"
+    key="your-object-key"
+    url=$(aws s3 presign s3://$bucket/$key)
+    echo "Pre-signed URL: $url"
     EOT
   }
 }
