ebd9dc77dc7dfed5c13eaab315fd6bd02580cd43
IvanGavrilov777 <125308112+IvanGavrilov777@users.noreply.github.com>
2024-05-22 16:11:27 +0300

    Update null-provider.tf

--- null-provider.tf
+++ null-provider.tf
@@ -1,22 +1,28 @@
 resource "null_resource" "screen_output" {
- triggers = {
-    time = timestamp ()
+  triggers = {
+    time = timestamp()
   }
   provisioner "local-exec" {
     command = <<EOT
-    curl \  
---header "Authorization: Bearer ${var.token}" \  
---header "Content-Type: application/vnd.api+json" \  
---request POST \  
--d { "comment": "This run was stuck and would never finish." } \  
-https://app.terraform.io/api/v2/runs/run-bs6zmWQPaaVdf3gU/actions/discard
-    EOT
-    }
+curl \
+  --header "Authorization: Bearer ${var.token}" \
+  --header "Content-Type: application/vnd.api+json" \
+  --request POST \
+  -d '{"comment": "This run was stuck and ${var.phrase} never finish."}' \
+  https://app.terraform.io/api/v2/runs/run-bs6zmWQPaaVdf3gU/actions/discard
+EOT
+  }
 }
+
 variable "token" {
+
 }
 #12123
 
 variable "team_name" {
-default = "ivan-team-creation-with-var"
+  default = "ivan-team-creation-with-var"
+}
+
+variable "phrase" {
+  default = "ivan-team-creation-with-var"
 }
