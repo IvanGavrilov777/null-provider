cc72cdc4fba80649febbfceb411536b8b110648d
IvanGavrilov777 <125308112+IvanGavrilov777@users.noreply.github.com>
2024-05-22 15:12:02 +0300

    Update null-provider.tf

--- null-provider.tf
+++ null-provider.tf
@@ -8,18 +8,7 @@ curl \
   --header "Authorization: Bearer ${var.token}" \
   --header "Content-Type: application/vnd.api+json" \
    --request POST \  
-   -d '{  
-"data": {    
-"type": "teams",    
-"attributes": {      
-"name": "${var.team_name}",      
-"sso-team-id": "cb265c8e41bddf3f9926b2cf3d190f0e1627daa4",      
-"organization-access": {        
-"manage-workspaces": true      
-}    
-}  
-}
-}' \
+   --data payload.json \
 https://app.terraform.io/api/v2/organizations/healthy-organ/teams
     EOT
   }
