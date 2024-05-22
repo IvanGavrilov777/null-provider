e5c9f69a16bd145b0e54100466d1bce86e25bb04
IvanGavrilov777 <125308112+IvanGavrilov777@users.noreply.github.com>
2024-05-22 15:15:18 +0300

    Update null-provider.tf

--- null-provider.tf
+++ null-provider.tf
@@ -9,7 +9,7 @@ curl \
   --header "Content-Type: application/vnd.api+json" \
    --request POST \  
    --data @payload.json \
-https://app.terraform.io/api/v2/organizations/healthy-organ/teams
+   "https://app.terraform.io/api/v2/organizations/healthy-organ/teams"
     EOT
   }
 }
